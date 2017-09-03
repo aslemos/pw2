3a0ceb07eba754278220b93a7516ac9e5a9e0cef
Majid <Majid@Utilisateur-PC>
2017-09-03 15:55:46 -0400

    J'ai fait des changements dans les fichiers suivant: 1. autoload (ajout de parametres dans : libraries; helper; model), 2. config (ajout d'une ligne 54 mais déactivée), 3. route ( ajout de plusieurs routes mais elles sont toutes déactivées).

--- www/application/config/routes.php
+++ www/application/config/routes.php
@@ -1,53 +1,47 @@
 <?php
+
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 /*
-| -------------------------------------------------------------------------
+| ----------------------------------------------
 | URI ROUTING
-| -------------------------------------------------------------------------
-| This file lets you re-map URI requests to specific controller functions.
-|
-| Typically there is a one-to-one relationship between a URL string
-| and its corresponding controller class/method. The segments in a
-| URL normally follow this pattern:
-|
-|	example.com/class/method/id/
-|
-| In some instances, however, you may want to remap this relationship
-| so that a different class/function is called than the one
-| corresponding to the URL.
-|
-| Please see the user guide for complete details:
-|
-|	https://codeigniter.com/user_guide/general/routing.html
-|
-| -------------------------------------------------------------------------
-| RESERVED ROUTES
-| -------------------------------------------------------------------------
-|
-| There are three reserved routes:
-|
-|	$route['default_controller'] = 'welcome';
-|
-| This route indicates which controller class should be loaded if the
-| URI contains no data. In the above example, the "welcome" class
-| would be loaded.
-|
-|	$route['404_override'] = 'errors/page_missing';
-|
-| This route will tell the Router which controller/method to use if those
-| provided in the URL cannot be matched to a valid route.
-|
-|	$route['translate_uri_dashes'] = FALSE;
-|
-| This is not exactly a route, but allows you to automatically route
-| controller and method names that contain dashes. '-' isn't a valid
-| class or method name character, so it requires translation.
-| When you set this option to TRUE, it will replace ALL dashes in the
-| controller and method URI segments.
-|
-| Examples:	my-controller/index	-> my_controller/index
-|		my-controller/my-method	-> my_controller/my_method
+| ----------------------------------------------
+*/
+
+/*
+$route['usagers/login'] = 'usagers/login';
+$route['usagers/logout'] = 'usagers/logout';
+$route['usagers/register'] = 'usagers/register';
+$route['usagers/update'] = 'usagers/update';
+$route['usagers/(:any)'] = 'usagers/view/$1';
+//$route['usagers/admin']  = 'usagers/admin';
+$route['usagers'] 	 = 'usagers/index';
+
+$route['vehicules/user'] = 'vehicules/user';
+$route['vehicules/create'] = 'vehicules/create';
+$route['vehicules/update'] = 'vehicules/update';
+$route['vehicules/vehicule_list'] = 'vehicules/vehicule_list';
+$route['vehicules/(:any)'] = 'vehicules/view/$1';
+$route['vehicules'] 	   = 'vehicules/index';
+
+$route['roles/create'] = 'roles/create';
+$route['roles/usagers/(:any)'] = 'roles/usagers/$1';
+$route['roles'] = 'roles/index';
+
+$route['marques/create'] = 'marques/create';
+$route['marques/update'] = 'marques/update';
+$route['marques/vehicules/(:any)'] = 'marques/vehicules/$1';
+$route['marques'] = 'marques/index';
+
+$route['modeles/create'] = 'modeles/create';
+$route['modeles/vehicules/(:any)'] = 'modeles/vehicules/$1';
+$route['modeles'] = 'modeles/index';
+
+
+$route['home/user'] = 'home/user';
+$route['home/vehicule'] = 'home/vehicule';
+$route['home/about'] = 'home/about';
+$route['default_controller'] = 'home/index';
 */
 $route['default_controller'] = 'accueil';
 $route['404_override'] = '';
