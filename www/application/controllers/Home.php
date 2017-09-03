16d9bca5af318b31bbc9de463dfee34c3a91554c
Majid <Majid@Utilisateur-PC>
2017-09-03 15:59:05 -0400

    J'ai fait des changements dans les fichiers suivant: A/ Dossier config :   1. autoload.php (ajout de parametres dans : libraries; helper; model),   2. config.php (ajout d'une ligne 54 mais déactivée),   3. route.php ( ajout de plusieurs routes mais elles sont toutes déactivées)
    B/ Dossier controller
      1. Ajout du controleur Home.php
    
    C/ Dans le dossier models:

---
+++ www/application/controllers/Home.php
@@ -0,0 +1,66 @@
+<?php
+/* 
+ * *** NB: la fonction "search" non encore focntionnelle ***
+ * 
+ * Nom de la classe: Home (Controleur par défaut)
+ * Déscription: Permet d'afficher la page d'accueil 
+ * et les pages contenues dans le dossier 'application/views/home/'.
+ * Liste des pages : index.php, vehicule.php, about.php.
+ * Liste des fonctions: 
+ * ** 1. index      => Affiche page d'accueil
+ * ** 2. about      => Affiche page À propos
+ * ** 3. vehicule   => Affiche liste des vehicules disponibles
+ * ** 4. search     => Affiche les véhicules selon le/les critère(s) de recherche
+ * 
+ */
+
+class Home extends CI_Controller {
+	
+    public function index() {
+        
+        $data['title'] = 'Liste des vehicules';
+        
+        $data['vehicules'] = $this->vehicule_model->get_vehicules();		
+        //print_r($data['vehicules']);
+
+        $this->load->view('templates/header');
+        $this->load->view('home/index', $data);
+        $this->load->view('templates/footer');
+    }
+	
+    public function about() {
+        
+        $data['title'] = 'À propos de nous';
+        
+        $this->load->view('templates/header');
+        $this->load->view('home/about', $data);
+        $this->load->view('templates/footer'); 
+    }
+    
+    public function vehicule() {
+        
+        $data['title'] = 'Lite des véhicules';
+        
+        $data['vehicules'] = $this->vehicule_model->get_vehicules();
+        
+        $this->load->view('templates/header');
+        $this->load->view('home/vehicule', $data);
+        $this->load->view('templates/footer'); 
+    }
+    
+    public function search (){
+
+    $data['title'] = 'Formulaire de recherche';
+
+    $data['usagers'] = $this->vehicule_model->get_usagers();
+    $data['type_vehicules'] = $this->vehicule_model->get_vehicules();
+    $data['marques'] = $this->vehicule_model->get_marques();
+    $data['modeles'] = $this->vehicule_model->get_modeles();
+    $data['carburants'] = $this->vehicule_model->get_carburants();
+    $data['transmissions'] = $this->vehicule_model->get_transmissions();
+    $data['arrondissements'] = $this->vehicule_model->get_arrondissements();
+
+    $this->vehicule_model->search_vehicule($search_field);
+    redirect('vehicules');
+    }
+}
