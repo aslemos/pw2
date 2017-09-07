98d4faece633953261c3bb49c67c970024642789
asl <asl@a-PC>
2017-09-07 16:08:30 -0400

    - Suppression du paramètre "index_page" (mis à nul)

--- www/application/config/config.php
+++ www/application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://proj2.local:8081/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = ''; // index.php
 
 /*
 |--------------------------------------------------------------------------
