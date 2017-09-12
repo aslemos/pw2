7d6f656ba7959ceabfa206066581ef0440415d79
asl <asl@a-PC>
2017-09-12 18:01:59 -0400

    - Correction des appels du contrôleur de réclamations

--- www/application/views/membre/historique_locataires.php
+++ www/application/views/membre/historique_locataires.php
@@ -1,5 +1,4 @@
 <?php
-
 $meta_keywords = "";
 $meta_description = "";
 $page_title = "historique des locations";
@@ -8,38 +7,38 @@ $body_class = "subpages membre";
 // Header
 include VIEWPATH . '/common/header.php';
 //========================================================
-include VIEWPATH .'client/boutons_client.php';
+include VIEWPATH . 'client/boutons_client.php';
 ?>
 <h2>Historique des locataires</h2>
 <form action="" name="formulaire" id="form-demandes-id">
-	<div class="table-responsive">
-	<label>Choisir un usager  :</label>
-	<select name="Usagers">
-		<option>Sélectionner</option>
-        <!--mettre les données dynamiquement dans la liste de voitures-->
-        <?php foreach ($usagers as $usager) { ?>
-		<option value="<?=$usager['user_id'];?>"><?=$usager['prenom']; ?></option>
-        <?php } ?>
-	</select>
-	<label>Periode</label>
-                <div class="form-group">
-                    <label class="control-label col-xs-3">De :</label>
-                    <div class='input-group date col-xs-6' >
-                        <input type='text' class="form-control" id='datetimepickerDe'/>
-                        <span class="input-group-addon" >
-                            <span class="glyphicon glyphicon-calendar"></span>
-                        </span>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="control-label col-xs-3">à :</label>
-                    <div class='input-group date col-xs-6' >
-                        <input type='text' class="form-control" id='datetimepickerA'/>
-                        <span class="input-group-addon" >
-                            <span class="glyphicon glyphicon-calendar"></span>
-                        </span>
-                    </div>
-                </div>
+    <div class="table-responsive">
+        <label>Choisir un usager  :</label>
+        <select name="Usagers">
+            <option>Sélectionner</option>
+            <!--mettre les données dynamiquement dans la liste de voitures-->
+            <?php foreach ($usagers as $usager) { ?>
+                <option value="<?= $usager['user_id']; ?>"><?= $usager['prenom']; ?></option>
+            <?php } ?>
+        </select>
+        <label>Periode</label>
+        <div class="form-group">
+            <label class="control-label col-xs-3">De :</label>
+            <div class='input-group date col-xs-6' >
+                <input type='text' class="form-control" id='datetimepickerDe'/>
+                <span class="input-group-addon" >
+                    <span class="glyphicon glyphicon-calendar"></span>
+                </span>
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="control-label col-xs-3">à :</label>
+            <div class='input-group date col-xs-6' >
+                <input type='text' class="form-control" id='datetimepickerA'/>
+                <span class="input-group-addon" >
+                    <span class="glyphicon glyphicon-calendar"></span>
+                </span>
+            </div>
+        </div>
 
         <div class="row btn-liens" >
             <!-- Boutton pour afficher l'historique des location pour un membre -->
@@ -47,57 +46,56 @@ include VIEWPATH .'client/boutons_client.php';
         </div>
 
 
-	<table class="table">
-		<thead>
-			<tr>
-				<th class="">Nº</th>
-				<th class="">Marque</th>
-				<th class="">Modèle</th>
-				<th class="">Matricule</th>
-                                <th class="">Date debut</th>
-                                <th class="">Date fin</th>
-				<th class="">Année</th>
-				<th class="">Nombre de <br />jours loué</th>
-				<th class="">Montant<br />total</th>
-                                <th class="">Reclamation voiture</th>
-			</tr>
-		</thead>
-		<tbody>
-            <!--mettre les données dynamiquement dans la liste de locations-->
-            <?php foreach ($locations as $location) {
-               // var_dump($locations);
-                //var_dump($location);
-                // À FAIRE :
-                // - calculer le nombre de jours (date finale - date initiale)
-                $diff = abs(strtotime($location['date_fin']) - strtotime($location['date_debut']));
-                $nb_jours = (int)floor($diff  / (60*60*24)) + 1;
-                //$years = floor($diff / (365*60*60*24));
-                //$months = floor(($diff - $years * 365*60*60*24) / (30*60*60*24));
-                //$nb_jours = floor(($diff - $years * 365*60*60*24 - $months*30*60*60*24)/ (60*60*24));
-                
-                //var_dump($diff, $months);
+        <table class="table">
+            <thead>
+                <tr>
+                    <th class="">Nº</th>
+                    <th class="">Marque</th>
+                    <th class="">Modèle</th>
+                    <th class="">Matricule</th>
+                    <th class="">Date debut</th>
+                    <th class="">Date fin</th>
+                    <th class="">Année</th>
+                    <th class="">Nombre de <br />jours loué</th>
+                    <th class="">Montant<br />total</th>
+                    <th class="">Reclamation voiture</th>
+                </tr>
+            </thead>
+            <tbody>
+                <!--mettre les données dynamiquement dans la liste de locations-->
+                <?php
+                foreach ($locations as $location) {
+                    // var_dump($locations);
+                    //var_dump($location);
+                    // À FAIRE :
+                    // - calculer le nombre de jours (date finale - date initiale)
+                    $diff = abs(strtotime($location['date_fin']) - strtotime($location['date_debut']));
+                    $nb_jours = (int) floor($diff / (60 * 60 * 24)) + 1;
+                    //$years = floor($diff / (365*60*60*24));
+                    //$months = floor(($diff - $years * 365*60*60*24) / (30*60*60*24));
+                    //$nb_jours = floor(($diff - $years * 365*60*60*24 - $months*30*60*60*24)/ (60*60*24));
+                    //var_dump($diff, $months);
+                    //echo  $nb_jours
+                    // printf("%d years, %d months, %d days\n", $years, $months, $days);
 
-                //echo  $nb_jours 
-               // printf("%d years, %d months, %d days\n", $years, $months, $days);
-                
-                $valeur_total = $location['prix'] * $nb_jours;
-                ?>
-			<tr>
-				<td class="">1</td>
-				<td class=""><?=$location['nom_marque'];?></td>
-				<td class=""><?=$location['nom_modele'];?></td>
-				<td class=""><?=$location['matricule'];?></td>
-                                <td class=""><?=$location['date_debut'];?></td>
-                                <td class=""><?=$location['date_fin'];?></td>                              
-				<td class=""><?=$location['annee'];?></td>
-				<td class=""><?=$nb_jours;?></td>
-				<td class=""><?=$valeur_total;?></td>
-                                <td class=""><a class="btn btn-inline" href="<?=$base_url?>reclamation/form_locataire/<?=$location['location_id']?>"></a></td>
-			</tr>
-            <?php } ?>
-		</tbody>
-	</table>
-	</div>
+                    $valeur_total = $location['prix'] * $nb_jours;
+                    ?>
+                    <tr>
+                        <td class="">1</td>
+                        <td class=""><?= $location['nom_marque']; ?></td>
+                        <td class=""><?= $location['nom_modele']; ?></td>
+                        <td class=""><?= $location['matricule']; ?></td>
+                        <td class=""><?= $location['date_debut']; ?></td>
+                        <td class=""><?= $location['date_fin']; ?></td>
+                        <td class=""><?= $location['annee']; ?></td>
+                        <td class=""><?= $nb_jours; ?></td>
+                        <td class=""><?= $valeur_total; ?></td>
+                        <td class=""><a class="btn btn-inline" href="<?= $base_url ?>reclamation/form_locataire/<?= $location['user_id'] ?>"></a></td>
+                    </tr>
+<?php } ?>
+            </tbody>
+        </table>
+    </div>
 </form>
 <!-- Div pour affichage -->
 <div id="resultat"></div>
@@ -105,18 +103,18 @@ include VIEWPATH .'client/boutons_client.php';
 
 <script>
 
-                /*Annee de voiture*/
-                $(function () {
-                    $('#datetimepickerDe').datepicker({   
-                        
-                  });
-                });
+    /*Annee de voiture*/
+    $(function () {
+        $('#datetimepickerDe').datepicker({
 
-                $(function () {
-                    $('#datetimepickerA').datepicker();
-                });
-                          
-                
+        });
+    });
+
+    $(function () {
+        $('#datetimepickerA').datepicker();
+    });
+
+
 </script>
 
 <?php
