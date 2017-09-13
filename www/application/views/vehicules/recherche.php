36b99b71835be50d6eea4eb592bd0ec652266ba6
iadhy <iadhy@users.github.com>
2017-09-13 15:59:48 -0400

    Modifier l'affichage dynamiques des véhicules

--- www/application/views/vehicules/recherche.php
+++ www/application/views/vehicules/recherche.php
@@ -196,16 +196,24 @@ include VIEWPATH . 'common/header.php';
 
 
             <div class="cols7 col-md-8 col-sm-12 col-xs-12">
-                <?php foreach ($resultat as $vehicule) { ?>
+                <?php 
+                echo '<pre>';
+                var_dump($resultat);
+                echo '</pre>';
+                ?>
+                
+                <?php 
+                foreach ($resultat as $vehicule) { ?>
                 <div class="cols8 col-md-12 col-sm-12 col-xs-12">
                     <div class="blocks">
                         <div class="img-container">
                             <img src="<?=$base_url?>assets/images/header/bg_header.jpg" alt="">
                         </div>
+                        <div>
+                            <h3> <?= $vehicule['annee'] ?> <?= $vehicule['description'] ?> <?= $vehicule['description'] ?> <?= $vehicule['description'] ?> </h3>
+                        </div>
                         <div class="desc-container">
-                            <p>Mauris pellentesque vestibulum suscipit. Suspendisse potenti. Nunc eget maximus ipsum. Quisque sed sagittis nisi. Donec rutrum commodo tortor, sit amet condimentum quam rutrum quis. Proin et posuere enim. Nullam sodales diam at tincidunt placerat. Phasellus ornare velit nunc, non tristique magna auctor sit amet. Duis ut justo eget eros hendrerit semper. Nunc fringilla nunc eget est interdum, non tempus massa sodales. Suspendisse eget libero leo. Vivamus nec pretium nibh, ac facilisis metus. Aliquam dui metus, vehicula id malesuada non, sollicitudin sit amet orci. Vestibulum efficitur ullamcorper pulvinar.</p>
-                            <p>Pellentesque rhoncus fringilla libero, a blandit mauris rhoncus non. Pellentesque arcu dolor, rutrum sit amet nunc a, rutrum aliquet nulla. Ut pharetra dui ipsum, non dapibus velit sagittis nec. Mauris mollis posuere sapien, eu accumsan urna rhoncus vel. Vestibulum at suscipit libero. In efficitur risus nec nisi dignissim, nec feugiat massa dignissim. Integer nec urna id est varius eleifend ac et augue. Curabitur efficitur purus nec luctus posuere. Morbi ultricies tellus quis dolor pulvinar pharetra. Cras ultricies leo lectus, eget faucibus lectus vehicula maximus. Maecenas porta leo sem, quis facilisis lectus varius ac.</p>
-                            <p>Aliquam mollis sit amet velit eleifend cursus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Pellentesque maximus sem ut ipsum placerat, a elementum tortor iaculis. Etiam pretium sodales libero eget varius. Nulla ut lorem metus. Sed sodales ullamcorper egestas. Ut euismod fermentum aliquam. Donec volutpat pharetra vehicula. Mauris dignissim, ex a accumsan euismod, nisl quam placerat diam, eget luctus leo nisl eget orci. Maecenas et vulputate ex, egestas lacinia nisl. Suspendisse potenti. Donec risus turpis, tincidunt sit amet fringilla in, sollicitudin id enim.</p>
+                          <?= $vehicule['description'] ?>
                         </div>
                     </div>
                 </div>
