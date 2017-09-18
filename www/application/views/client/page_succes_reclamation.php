ca5ec7c3f42743756f11bcf6e36372359dffde0a
user <user@A>
2017-09-18 00:10:55 -0600

    ajouter page succes reclamation

---
+++ www/application/views/client/page_succes_reclamation.php
@@ -0,0 +1,14 @@
+<?php include VIEWPATH . '/common/header.php'; ?>
+
+<body>
+    <div class="form-style-10">
+        <h3>Félicitations!<span>Votre reclamation a été envoyée !</span></h3>
+        <h3>Veuillez attendre réponse du admin</h3>
+        <div class="btn-liens" >
+            <!-- Bouton pour afficher redirect apres succes -->
+            <a class="btn btn-success" href="<?= $base_url ?>vehicule/recherche" >OK</a>
+        </div>
+    </div>
+</body>
+
+<?php include VIEWPATH . '/common/footer.php'; ?>
