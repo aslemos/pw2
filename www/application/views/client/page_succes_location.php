cf515c52f84c50429ebf45677dd017ddda960e18
user <user@A>
2017-09-19 04:10:14 -0600

    insert dans table payemant

--- www/application/views/client/page_succes_location.php
+++ www/application/views/client/page_succes_location.php
@@ -3,7 +3,7 @@
 <body>
     <div class="form-style-10">
         <h3>Félicitations!<span>Vous avez réservé une voiture!</span></h3>
-        <h3>Veuillez attendre approbation du propriétaire</h3>
+        <h4>Veuillez attendre approbation du propriétaire</h4>
         <div class="btn-liens" >
             <!-- Bouton pour afficher redirect apres succes -->
             <a class="btn btn-success" href="<?= $base_url ?>vehicule/recherche" >OK</a>
