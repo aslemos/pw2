e7f3aa23e5559acca0010c03589046fb0b5cc5a6
user <user@A>
2017-09-19 20:02:34 -0600

    insert dans table payemant

--- www/application/views/client/page_succes_payemant.php
+++ www/application/views/client/page_succes_payemant.php
@@ -5,16 +5,13 @@
 </style>
 <body>
     <div class="form-style-10">
-        <h3 id="succes">Félicitations!
+        <h3 class="succes">Félicitations!
             <span>Paiement effectué avec succès!</span>
             <h4>Numero du paiement :
                 <?php echo $location_id; ?>
             </h4>
         </h3>
         <h4>Merci d'utiliser notre site</h4>
-
-
-
         <div class="btn-liens" >
             <!-- Bouton pour afficher redirect apres succes -->
             <a class="btn btn-success" href="<?= $base_url ?>" >OK</a>
