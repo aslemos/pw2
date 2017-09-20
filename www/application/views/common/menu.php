2bcc21bb55a0b1dc5675095c3e7782a74d22d917
asl <asl@a-PC>
2017-09-20 00:33:28 -0400

    - Ajustements des titres des éléments <a> - Ajout du lien d'inscription au menu principal pour les non-membres (par Majid)

--- www/application/views/common/menu.php
+++ www/application/views/common/menu.php
@@ -1,19 +1,20 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 <nav>
     <ul class="menu">
-        <li class=""><a href="<?=$base_url?>" title="">ACCUEIL</a></li>
-        <li class=""><a href="<?=$base_url?>accueil/apropos#s" title="">À PROPOS</a></li>
-        <li class=""><a href="<?=$base_url?>vehicule/recherche#s" title="">LOUER UNE VOITURE</a></li>
+        <li class=""><a href="<?=$base_url?>" title="Page d'accueil">ACCUEIL</a></li>
+        <li class=""><a href="<?=$base_url?>accueil/apropos#s" title="Qui sommes nous">À PROPOS</a></li>
+        <li class=""><a href="<?=$base_url?>vehicule/recherche#s" title="Trouvez une voiture">LOUER UN VÉHICULE</a></li>
 <?php if (UserAcces::userIsLogged()) { ?>
-        <li class=""><a href="<?=$base_url?>membre#s" title="">GESTION DES VOITURES</a></li>
+        <li class=""><a href="<?=$base_url?>membre#s" title="Fonctionnalités de membre">GESTION DES VÉHICULES</a></li>
         <?php if (UserAcces::userIsAdmin()) { ?>
-        <li class=""><a href="<?=$base_url?>admin/listeMembres#s" title="">ADMINISTRATION</a></li>
+        <li class=""><a href="<?=$base_url?>admin/listeMembres#s" title="Fonctions d'administration du site">ADMINISTRATION</a></li>
         <?php } ?>
         <li class=""><a href="<?=$base_url?>messagerie#s" title="Service de messagerie">MESSAGERIE</a></li>
         <li class=""><a href="<?=$base_url?>usager/logout" title="Se déconnecter du système">LOGOUT</a></li>
 <?php } else { ?>
-        <li class=""><a href="<?=$base_url?>contact#s" title="">CONTACTEZ-NOUS</a></li>
-        <li class=""><a href="<?=$base_url?>usager/login#s" title="">SE CONNECTER</a></li>
+        <li class=""><a href="<?=$base_url?>contact#s" title="Entrez en contact avec nous">CONTACTEZ-NOUS</a></li>
+        <li class=""><a href="<?=$base_url?>usager/inscription#s" title="Devenez membre du service">S'INSCRIRE</a></li>
+        <li class=""><a href="<?=$base_url?>usager/login#s" title="Accéder au système">SE CONNECTER</a></li>
 <?php } ?>
 
         <li class="hidden-md hidden-lg"><button class="btn-menu" id="close-side-menu">FERMER</button></li>
